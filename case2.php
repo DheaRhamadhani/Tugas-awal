b1a1e407f1a8371f48a400fd33bf1f0affe64373
DheaRhamadhani <DheaRhamadhani@gmail.com>
2021-07-28 10:48:57 +0700

    upload tugas

--- case2.php
+++ case2.php
@@ -4,15 +4,25 @@
  * faktorial (5) -> 5* 4 * 3 * 2 * 1 = 120 
  */
 
- function faktorial ($x){
-     $angka = 1;
-     $faktorial = 1;
-     while ($angka <= $x){
-         $faktorial = $faktorial * $angka;
-         $angka = $angka + 1;
-     }
-     return $faktorial;
- }
+ /**
+  * fungsi -> void dan non-void
+  * void ->fungsi yg tdk mengembalikan value
+  * non-void -> fungsi yg mengembalikan value (return value)
 
- echo faktorial (5);
+  * analogi -> ketua kelas meminta info tentang saldo kas
+  * fungsi hitungSaldo()-> return saldonya
+
+  * analogi -> ketua kelas meminta info tentang saldo kas
+  * fungsi narikKas()
+  */
+function faktorial ($number){
+	$result = 1;
+	 for($i=$number; $i >= 1 ; $i--){
+		 $result = $result * $i;
+	 }
+	  //return $fresult;
+	  echo "Faktorial of $number is $result";
+	}
+// echo "Faktorial of 10 is " . faktorial(10);
+echo faktorial(5);
 ?>
